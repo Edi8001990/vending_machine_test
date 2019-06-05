4b6af727c26e2f09218bed0b2a6a3c6c9de38cc4
Mateusz <mateusz.trajdos@gmail.com>
2019-06-05 15:33:20 +0100

    Start

---
+++ test.js
@@ -0,0 +1,19 @@
+var VendingMachine = function () {
+    this.balance = 0;
+};
+
+VendingMachine.prototype.getBalance = function () {
+    return this.balance;
+};
+
+let assert = require('assert');
+
+describe('Vending Machine', function () {
+
+    describe('Balance of money inserted', function () {
+        it('Is zero when initially powered up', function () {
+            machine = new VendingMachine();
+            assert.strictEqual(0, machine.getBalance())
+        });
+    });
+});
